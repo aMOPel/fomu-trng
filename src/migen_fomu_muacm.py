b4d8a72c3bccc65fa12f71650b4065857ce26d43
aMOPel <momoPFL@gmail.com>
2022-06-09 10:45:04 +0200

    experimenting with the controller

--- src/migen_fomu_muacm.py
+++ src/migen_fomu_muacm.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# TODO: original authoren referenzieren
+
 # from migen import *
 from migen.genlib.resetsync import AsyncResetSynchronizer
 from migen.genlib.fsm import *
@@ -12,7 +14,7 @@ from migen import Module, Signal, ClockSignal, ClockDomain, If, \
 import no2migen
 
 
-class LoopbackTest(Module):
+class AcmController(Module):
 
     def __init__(self, platform):
         self.rst = Signal()
@@ -90,7 +92,7 @@ class LoopbackTest(Module):
 
         self.comb += [
             # muacm.in_data.eq(muacm.out_data),
-            # muacm.in_last.eq(0),
+            muacm.in_last.eq(0),
             # muacm.in_valid.eq(muacm.out_valid),
             # muacm.out_ready.eq(muacm.in_ready),
             muacm.out_ready.eq(1),
@@ -104,8 +106,6 @@ class LoopbackTest(Module):
 
         FRAME_SIZE_BITS = 7
         counter = Signal(FRAME_SIZE_BITS)
-        end_soon = Signal(1)
-        frame_done = Signal(1)
 
         fsm.act(
             "IDLE",
@@ -113,11 +113,9 @@ class LoopbackTest(Module):
             green_pwm.eq(0),
             blue_pwm.eq(0),
             NextValue(counter, 0),
-            NextValue(end_soon, 0),
-            NextValue(frame_done, 0),
             NextValue(muacm.in_data, 0),
             NextValue(muacm.in_valid, 0),
-            NextValue(muacm.in_last, 0),
+            # NextValue(muacm.in_last, 0),
             NextValue(muacm.in_flush_now, 1),
             If(
                 muacm.out_data == self.START_CHAR,
@@ -127,30 +125,26 @@ class LoopbackTest(Module):
 
         fsm.act(
             "SENDING",
+            red_pwm.eq(0),
+            green_pwm.eq(0),
+            blue_pwm.eq(0),
             NextValue(muacm.in_flush_now, 0),
             # NextValue(muacm.in_last, 0),
             If(
-                end_soon & frame_done,
+                muacm.out_data == self.END_CHAR,
                 NextValue(counter, 0),
                 NextValue(muacm.in_data, 0),
                 NextValue(muacm.in_valid, 0),
+                # NextValue(muacm.in_last, 1),
+                # NextValue(muacm.in_flush_now, 1),
                 NextState("IDLE"),
             ).Elif(
                 muacm.in_ready,
                 NextValue(counter, counter + 1),
                 NextValue(muacm.in_data, counter),
                 NextValue(muacm.in_valid, 1),
-            ),
-            If(
-                muacm.out_data == self.END_CHAR,
-                NextValue(end_soon, 1),
-            ),
-            If(
-                counter == ((2**FRAME_SIZE_BITS) - 1),
-                NextValue(frame_done, 1),
-                NextValue(muacm.in_last, 1),
-            ).Else(
-                NextValue(muacm.in_last, 0),
+                # NextValue(muacm.in_last, 0),
+                # NextValue(muacm.in_flush_now, 0),
             ),
         )
 
